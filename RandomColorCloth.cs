1b9df286558e35b21ac95309489b25361793959c
LWM <lilmouse@littlewhitemouse.net>
2019-05-18 11:16:45 -0400

    Initial commit for 1.0

--- RandomColorCloth.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-namespace AreaRugs {
-    public class MyClass {
-        public MyClass() {
-        }
-    }
-}
---
+++ Source/RandomColorCloth.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+
+namespace LWM.AreaRugs {
+    public class RandomColoredCloth : ThingWithComps {
+        static RandomColoredCloth() {
+            coloredCloths=new List<ThingDef>();
+            coloredCloths.Add(DefDatabase<ThingDef>.GetNamed("RedCloth"));
+            coloredCloths.Add(DefDatabase<ThingDef>.GetNamed("BlueCloth"));
+        }
+        public override void PostMake() {
+            this.def=coloredCloths[rng.Next(2)];
+            base.PostMake();
+        }
+
+        static Random rng=new Random();
+        public static List<ThingDef> coloredCloths;
+    }
+}
